57943755f3b90de086c72dbaffb5898fe10a2706
Hanna Eismant <Hanna_Eismant@epam.com>
2017-10-30 11:05:29 +0300

    Create empty backoffice extension

---
+++ course/gensrc/com/epam/course/constants/GeneratedCourseConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 30, 2017 10:54:39 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.course.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCourseConstants
+{
+	public static final String EXTENSIONNAME = "course";
+	
+	protected GeneratedCourseConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
